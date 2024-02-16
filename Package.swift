54d526edcd439eab07ecba5d63ff3f95001ca25b
Elaine Ku <elaine.ku@zoom.us>
2024-02-16 13:55:08 -0800

    v1.10.5

--- Package.swift
+++ Package.swift
@@ -6,13 +6,22 @@ import PackageDescription
 let package = Package(
     name: "ZoomVideoSDK-iOS",
     platforms: [
-            .iOS(.v11)
+            .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ZoomVideoSDK-iOS",
             targets: ["ZoomVideoSDK"]),
+        .library(
+            name: "ZoomVideoSDK-iOS-CptShare",
+            targets: ["ZoomVideoSDK-CptShare"]),
+        .library(
+            name: "ZoomVideoSDK-iOS-Annotation",
+            targets: ["ZoomVideoSDK-Annotation"]),
+        .library(
+            name: "ZoomVideoSDK-iOS-zoomcml",
+            targets: ["ZoomVideoSDK-zoomcml"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,5 +33,14 @@ let package = Package(
         .binaryTarget(
                     name: "ZoomVideoSDK",
                     path: "ZoomVideoSDK.xcframework"),
+        .binaryTarget(
+                    name: "ZoomVideoSDK-CptShare",
+                    path: "CptShare.xcframework"),
+        .binaryTarget(
+                    name: "ZoomVideoSDK-Annotation",
+                    path: "zm_annoter_dynamic.xcframework"),
+        .binaryTarget(
+                    name: "ZoomVideoSDK-zoomcml",
+                    path: "zoomcml.xcframework"),
     ]
 )
---
+++ zm_annoter_dynamic.xcframework/ios-arm64/zm_annoter_dynamic.framework/Headers/zm_annoter_dynamic.h
@@ -0,0 +1,21 @@
+//
+//  zm_annoter_dynamic.h
+//  zm_annoter_dynamic
+//
+//  Created by Owen Hu on 2023/4/23.
+//  Copyright © 2023 Zoom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for zm_annoter_dynamic.
+FOUNDATION_EXPORT double zm_annoter_dynamicVersionNumber;
+
+//! Project version string for zm_annoter_dynamic.
+FOUNDATION_EXPORT const unsigned char zm_annoter_dynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework
+// using statements like #import <zm_annoter_dynamic/PublicHeader.h>
+
+#import <zm_annoter_dynamic/anno.h>
+#import <zm_annoter_dynamic/anno_base.h>
