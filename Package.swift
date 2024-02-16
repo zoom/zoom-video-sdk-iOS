8828c76f8845f13f5cd20500249406d9c1659d71
Elaine Ku <elaine.ku@zoom.us>
2024-02-16 14:11:07 -0800

    v1.7.10

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ZoomVideoSDK-iOS",
-            targets: ["ZoomVideoSDK", "zoomcml", "CptShare"]),
+            name: "ZoomVideoSDK-iOS-CptShare",
+            targets: ["CptShare"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,12 +21,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(
-                    name: "ZoomVideoSDK",
-                    path: "ZoomVideoSDK.xcframework"),
-        .binaryTarget(
-                    name: "zoomcml",
-                    path: "zoomcml.xcframework"),
         .binaryTarget(
                     name: "CptShare",
                     path: "CptShare.xcframework"),
